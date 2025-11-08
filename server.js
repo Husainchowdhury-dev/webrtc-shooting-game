c95a1ab8866ee231cdcc270edfe04131a0ba04b1
Husainchowdhury-dev <husainchowdhury923@gmail.com>
2025-11-07 19:30:08 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("New player connected:", socket.id);
+
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("candidate", (data) => socket.broadcast.emit("candidate", data));
+});
+
+server.listen(3000, () => console.log("🚀 Server running at http://localhost:3000"));
